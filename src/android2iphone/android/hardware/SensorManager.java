50e10e60095bb44cfa1fe6b892719b93356fc70b
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-04-14 22:38:16 +0000

    Report accelerometer readings correctly. Readings are dependent on device orientation.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@183 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/android2iphone/android/hardware/SensorManager.java
+++ src/android2iphone/android/hardware/SensorManager.java
@@ -24,53 +24,59 @@ import org.xmlvm.iphone.UIAcceleration;
 import org.xmlvm.iphone.UIAccelerometerDelegate;
 import org.xmlvm.iphone.UIAccelerometer;
 
+import android.app.ActivityWrapper;
+import android.content.pm.ActivityInfo;
+
 public class SensorManager implements UIAccelerometerDelegate {
-  public static final float GRAVITY_EARTH = 9.80665f;
-  public static final int SENSOR_ACCELEROMETER = 0x00000002;
-  public static final int SENSOR_DELAY_FASTEST = 0x00000000;
-  // TODO: support list on iPhone
-  private RegisteredListener[] listeners = new RegisteredListener[20];
-  int numListeners;
+    public static final float    GRAVITY_EARTH        = 9.80665f;
+    public static final int      SENSOR_ACCELEROMETER = 0x00000002;
+    public static final int      SENSOR_DELAY_FASTEST = 0x00000000;
+    // TODO: support list on iPhone
+    private RegisteredListener[] listeners            = new RegisteredListener[20];
+    int                          numListeners;
 
-  private UIAccelerometer accel;
+    private UIAccelerometer      accel;
 
-  public SensorManager() {
-    accel = UIAccelerometer.getSharedAccelerometer();
-    accel.setUpdateInterval(1.0 / 40);
-    accel.setDelegate(this);
-  }
-
-  public void registerListener(SensorListener listener, int sensors, int rate) {
-    listeners[numListeners++] = new RegisteredListener(listener, sensors);
-  }
-
-  public void accelerometerDidAccelerate(UIAccelerometer accelerometer,
-			UIAcceleration acceleration) {
-    // This is to adapt the iPhone value range to the Android one. iPhone/iPod
-    // touch scale 1G to a value of 1 whereas the Android phone delivers the
-    // actual G-force value.
-    float x = (float) (acceleration.y() * GRAVITY_EARTH);
-    float y = (float) -(acceleration.x() * GRAVITY_EARTH);
-    float z = (float) (acceleration.z() * GRAVITY_EARTH);
-    for (int i = 0; i < numListeners; i++) {
-      RegisteredListener listener = listeners[i];
-      if ((listener.sensors & SENSOR_ACCELEROMETER) != 0) {
-        // For some reason Android (HTC Dream) gives the values twice in a
-        // 6-element array.
-        // TODO: Not sure if these values are in the correct order.
-        listener.listener.onSensorChanged(SENSOR_ACCELEROMETER, new float[] {
-            x, y, z, x, y, z });
-      }
+    public SensorManager() {
+        accel = UIAccelerometer.getSharedAccelerometer();
+        accel.setUpdateInterval(1.0 / 40);
+        accel.setDelegate(this);
     }
-  }
+
+    public void registerListener(SensorListener listener, int sensors, int rate) {
+        listeners[numListeners++] = new RegisteredListener(listener, sensors);
+    }
+
+    public void accelerometerDidAccelerate(UIAccelerometer accelerometer,
+            UIAcceleration acceleration) {
+        // This is to adapt the iPhone value range to the Android one.
+        // iPhone/iPod
+        // touch scale 1G to a value of 1 whereas the Android phone delivers the
+        // actual G-force value.
+        float x = (float) (acceleration.x() * GRAVITY_EARTH);
+        float y = (float) (acceleration.y() * GRAVITY_EARTH);
+        float z = (float) (acceleration.z() * GRAVITY_EARTH);
+        for (int i = 0; i < numListeners; i++) {
+            RegisteredListener listener = listeners[i];
+            if ((listener.sensors & SENSOR_ACCELEROMETER) != 0) {
+                float[] values;
+                if (ActivityWrapper.getActivity().getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+                    values = new float[] {x, y, z, x, y, z};
+                } else {
+                    values = new float[] {-y, x, z, x, y, z};
+                }
+                listener.listener.onSensorChanged(SENSOR_ACCELEROMETER, values);
+            }
+        }
+    }
 }
 
 class RegisteredListener {
-  SensorListener listener = null;
-  int sensors = 0;
+    SensorListener listener = null;
+    int            sensors  = 0;
 
-  public RegisteredListener(SensorListener listener, int sensors) {
-    this.listener = listener;
-    this.sensors = sensors;
-  }
+    public RegisteredListener(SensorListener listener, int sensors) {
+        this.listener = listener;
+        this.sensors = sensors;
+    }
 }
