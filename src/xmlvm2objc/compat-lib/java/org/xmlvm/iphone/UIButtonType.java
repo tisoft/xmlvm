36607429a648572f90d6f8c9f8d6304de1cc8f2a
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-05-04 19:56:26 +0000

    Class created
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@266 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

---
+++ src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/UIButtonType.java
@@ -0,0 +1,14 @@
+
+package org.xmlvm.iphone;
+
+
+public class UIButtonType
+{
+
+    public static final int UIButtonTypeCustom           = 0;
+    public static final int UIButtonTypeRoundedRect      = 1;
+    public static final int UIButtonTypeDetailDisclosure = 2;
+    public static final int UIButtonTypeInfoLight        = 3;
+    public static final int UIButtonTypeInfoDark         = 4;
+    public static final int UIButtonTypeContactAdd       = 5;
+}
