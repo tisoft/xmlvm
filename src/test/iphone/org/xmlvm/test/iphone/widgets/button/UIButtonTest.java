541b7e028edc64a46d551dc5f069ecc65b9b81e8
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-05-06 06:04:10 +0000

    Additional buttons fixed.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@275 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/test/iphone/org/xmlvm/test/iphone/widgets/button/UIButtonTest.java
+++ src/test/iphone/org/xmlvm/test/iphone/widgets/button/UIButtonTest.java
@@ -21,6 +21,38 @@ public class UIButtonTest extends UIApplication {
         title.setTextAlignment(UITextAlignment.UITextAlignmentCenter);
         mainView.addSubview(title);
 
+        UIButton roundedRectButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeRoundedRect);
+        roundedRectButton.setFrame(new CGRect(10, 10, 90, 60));
+        //roundedRectButton.setFont(new Font("Arial", Font.BOLD, 14));
+        roundedRectButton.setTitle("Rounded", UIControlState.UIControlStateNormal);
+        mainView.addSubview(roundedRectButton);
+        //mainView.bringSubviewToFront(roundedRectButton);
+
+        UIButton detailDisclosureButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeDetailDisclosure);
+        detailDisclosureButton.setFrame(new CGRect(115, 10, 90, 60));
+//        detailDisclosureButton.setFont(new Font("Times New Roman", Font.BOLD, 16));
+        detailDisclosureButton.setTitle("Detail", UIControlState.UIControlStateNormal);
+        mainView.addSubview(detailDisclosureButton);
+//        mainView.bringSubviewToFront(detailDisclosureButton);
+
+        UIButton contactAddButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeContactAdd);
+        contactAddButton.setFrame(new CGRect(220, 10, 90, 60));
+        contactAddButton.setTitle("Contact", UIControlState.UIControlStateNormal);
+        mainView.addSubview(contactAddButton);
+//        mainView.bringSubviewToFront(contactAddButton);
+        
+        UIButton infoLightButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeInfoLight);
+        infoLightButton.setFrame(new CGRect(10, 390, 90, 60));
+        infoLightButton.setTitle("Light", UIControlState.UIControlStateNormal);
+        mainView.addSubview(infoLightButton);
+//        mainView.bringSubviewToFront(infoLightButton);
+
+        UIButton infoDarkButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeInfoDark);
+        infoDarkButton.setFrame(new CGRect(220, 390, 90, 60));
+        infoDarkButton.setTitle("Dark", UIControlState.UIControlStateNormal);
+        mainView.addSubview(infoDarkButton);
+//        mainView.bringSubviewToFront(infoDarkButton);
+
         /*
         UIButton customButton = new UIButton(UIButtonType.UIButtonTypeCustom, new CGRect(10, 10,
                 90, 60));
@@ -31,40 +63,6 @@ public class UIButtonTest extends UIApplication {
         mainView.addSubview(customButton);
         mainView.bringSubviewToFront(customButton);
     */
-        UIButton roundedRectButton = UIButton.buttonWithType(UIButtonType.UIButtonTypeRoundedRect);
-        roundedRectButton.setFrame(new CGRect(115, 10, 90, 60));
-        //roundedRectButton.setFont(new Font("Arial", Font.BOLD, 14));
-        roundedRectButton.setTitle("Rounded", UIControlState.UIControlStateNormal);
-        mainView.addSubview(roundedRectButton);
-        //mainView.bringSubviewToFront(roundedRectButton);
-
-        /*
-        UIButton detailDisclosureButton = new UIButton(UIButtonType.UIButtonTypeCustom, new CGRect(
-                220, 10, 90, 60));
-        detailDisclosureButton.setFont(new Font("Times New Roman", Font.BOLD, 16));
-        detailDisclosureButton.setTitle("Detail", UIControlState.UIControlStateNormal);
-        mainView.addSubview(detailDisclosureButton);
-        mainView.bringSubviewToFront(detailDisclosureButton);
-
-        UIButton infoLightButton = new UIButton(UIButtonType.UIButtonTypeCustom, new CGRect(10,
-                390, 90, 60));
-        infoLightButton.setTitle("Light", UIControlState.UIControlStateNormal);
-        mainView.addSubview(infoLightButton);
-        mainView.bringSubviewToFront(infoLightButton);
-
-        UIButton infoDarkButton = new UIButton(UIButtonType.UIButtonTypeCustom, new CGRect(115,
-                390, 90, 60));
-        infoDarkButton.setTitle("Dark", UIControlState.UIControlStateNormal);
-        mainView.addSubview(infoDarkButton);
-        mainView.bringSubviewToFront(infoDarkButton);
-
-        UIButton contactAddButton = new UIButton(UIButtonType.UIButtonTypeCustom, new CGRect(220,
-                390, 90, 60));
-        contactAddButton.setTitle("Contact", UIControlState.UIControlStateNormal);
-        mainView.addSubview(contactAddButton);
-        mainView.bringSubviewToFront(contactAddButton);
-    */
-        
         window.makeKeyAndVisible();
     }
 
