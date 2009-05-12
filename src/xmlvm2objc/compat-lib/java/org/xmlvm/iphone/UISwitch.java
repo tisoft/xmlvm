da1ddbfa1cdc0399841778552c6baf688ed9b208
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-05-12 18:14:27 +0000

    Event hadnling updated to UIResponder style
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@304 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/UISwitch.java
+++ src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/UISwitch.java
@@ -1,4 +1,3 @@
-
 package org.xmlvm.iphone;
 
 import java.awt.Color;
@@ -6,64 +5,41 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.util.Set;
 
-import org.xmlvm.iphone.internal.*;
-
-
-
-public class UISwitch
-    extends UIControl
-    implements GestureListener
-{
+public class UISwitch extends UIControl {
 
     private boolean   isOn;
     final private int INSET = 8;
-    private Font font;
+    private Font      font;
 
-
-
-    public UISwitch()
-    {
+    public UISwitch() {
         super(new CGRect(0, 0, 0, 0));
         init();
-	}
+    }
 
-
-
-    public UISwitch(CGRect rect)
-    {
+    public UISwitch(CGRect rect) {
         super(rect);
         init();
     }
 
-
-
-	private void init() {
-        Simulator.addGestureListener(this);
-		// Set a default font
-		font = new Font("Arial", Font.BOLD, 16);
-		this.setOn(false);
-	}
-
-
-	public void setOn(boolean on)
-    {
-    	this.isOn = on;
-    }
-    
-    
-    
-    public boolean isOn()
-    {
-    	return isOn;
+    private void init() {
+        // Set a default font
+        font = new Font("Arial", Font.BOLD, 16);
+        this.setOn(false);
     }
 
+    public void setOn(boolean on) {
+        this.isOn = on;
+    }
 
+    public boolean isOn() {
+        return isOn;
+    }
 
-    public void drawRect(CGRect rect)
-    {
+    public void drawRect(CGRect rect) {
         setTransformForThisView();
-        
+
         Graphics2D g = CGContext.theContext.graphicsContext;
         g.setFont(font);
         CGRect displayRect = getDisplayRect();
@@ -71,45 +47,26 @@ public class UISwitch
         int y = (int) displayRect.origin.y;
         int w = (int) displayRect.size.width;
         int h = (int) displayRect.size.height;
-        GradientPaint blueGradient = new GradientPaint(
-                0,
-                y,
-                new Color(61, 89, 171),
-                0,
-                y + h,
+        GradientPaint blueGradient = new GradientPaint(0, y, new Color(61, 89, 171), 0, y + h,
                 new Color(100, 149, 237));
-        GradientPaint whiteGradient = new GradientPaint(
-                0,
-                y,
-                new Color(220, 220, 220),
-                0,
-                y + h,
+        GradientPaint whiteGradient = new GradientPaint(0, y, new Color(220, 220, 220), 0, y + h,
                 Color.WHITE);
-        GradientPaint grayGradient = new GradientPaint(
-                0,
-                y,
-                Color.LIGHT_GRAY,
-                0,
-                y + h,
+        GradientPaint grayGradient = new GradientPaint(0, y, Color.LIGHT_GRAY, 0, y + h,
                 Color.WHITE);
         g.setPaint(isOn ? blueGradient : whiteGradient);
-        g.fillRoundRect(x, y,
-                w, h,
-                INSET, INSET);
+        g.fillRoundRect(x, y, w, h, INSET, INSET);
         g.setColor(Color.LIGHT_GRAY);
-        g.drawRoundRect(x, y,
-                w, h,
-                INSET, INSET);
+        g.drawRoundRect(x, y, w, h, INSET, INSET);
         CGRect knob = new CGRect(displayRect);
         float halfWidth = w / 2;
         knob.size.width = halfWidth;
         if (isOn)
             knob.origin.x += halfWidth;
-        g.drawRoundRect((int) knob.origin.x, (int) knob.origin.y,
-                (int) knob.size.width, (int) knob.size.height, INSET, INSET);
+        g.drawRoundRect((int) knob.origin.x, (int) knob.origin.y, (int) knob.size.width,
+                (int) knob.size.height, INSET, INSET);
         g.setPaint(grayGradient);
-        g.fillRoundRect((int) knob.origin.x, (int) knob.origin.y,
-                (int) knob.size.width, (int) knob.size.height, INSET, INSET);
+        g.fillRoundRect((int) knob.origin.x, (int) knob.origin.y, (int) knob.size.width,
+                (int) knob.size.height, INSET, INSET);
         String label = "ON";
         knob.origin.x = x;
         g.setColor(Color.WHITE);
@@ -119,36 +76,24 @@ public class UISwitch
             label = "OFF";
         }
         FontMetrics fm = g.getFontMetrics();
-        g.drawString(label, knob.origin.x + INSET, knob.origin.y
-                + (h - fm.getHeight()) / 2 + fm.getHeight() - fm.getDescent());
-        
+        g.drawString(label, knob.origin.x + INSET, knob.origin.y + (h - fm.getHeight()) / 2
+                + fm.getHeight() - fm.getDescent());
+
         restoreLastTransform();
     }
 
+    @Override
+    public void touchesEnded(Set<UITouch> touches, UIEvent event) {
+        UITouch t = touches.iterator().next();
+        CGPoint p = t.locationInView(this);
+        CGRect r = this.getBounds();
 
-
-    public void mouseClicked(int x, int y)
-    {
-        CGRect rect = getDisplayRect();
-        int inX = x - (int) rect.origin.x;
-        int inY = y - (int) rect.origin.y;
-        if (inX >= 0 && inX < frame.size.width / 2 && inY >= 0
-                && inY < frame.size.height)
-            setOn(true);
-        if (inX > frame.size.width / 2 && inX < frame.size.width
-                && inY >= 0 && inY < frame.size.height)
-            setOn(false);
-        Simulator.redrawDisplay();
+        if (!(p.x < 0 || p.y < 0 || p.x > r.size.width || p.y > r.size.height)) {
+            if (p.x <= r.size.width / 2) {
+                setOn(true);
+            } else {
+                setOn(false);
+            }
+        }
     }
-
-
-
-    public void gestureDragged(int dx, int dy)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-
-
 }
