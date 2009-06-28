a88ab20b03b20ec9e115c7ea48f57b1d073a38e8
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-06-28 23:23:12 +0000

    Simple Hello World Demo for Android
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@450 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

---
+++ demo/android/helloworld/src/org/xmlvm/demo/helloandroidworld/HelloWorld1.java
@@ -0,0 +1,29 @@
+package org.xmlvm.demo.helloandroidworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AbsoluteLayout;
+import android.widget.Button;
+
+/**
+ * A simple demo with which simple Android functionality can be tested.
+ */
+public class HelloWorld1 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AbsoluteLayout layout = new AbsoluteLayout(this);
+        Button okButton = new Button(this);
+        okButton.setText("Android Button");
+        AbsoluteLayout.LayoutParams p = new AbsoluteLayout.LayoutParams(280, 40,
+                20, 20);
+        okButton.setLayoutParams(p);
+        layout.addView(okButton);
+        setContentView(layout);
+    }
+    
+    public static void main(String[] args) {
+        (new HelloWorld1()).onCreate(null);
+    }
+}
