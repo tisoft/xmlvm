2db6e09e82746fbb04e4133c1851db69be3c3ddc
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-07-18 20:33:10 +0000

    Adding contribute site including CLA
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@472 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

---
+++ doc/homepage/contribute/cla.php
@@ -0,0 +1,226 @@
+<?php
+  include("../header.html");
+  include("../content_top.html");
+?>
+<style>
+pre {base-200612.css (line 68)
+  background-color:#F5F5F5;
+  border:1px solid silver;
+  margin:2em;
+  overflow:auto;
+  padding:0.5em;
+}
+pre, code {base-200612.css (line 62)
+  color:#007000;
+  font-family:"bogus font here",monospace;
+  font-size:100%;
+}
+</style>
+<h1>Individual Contributors License</h1>
+
+<h2 class="title">XMLVM<br/> 
+  Individual Contributor License Agreement ("Agreement"), v1.0</h2> 
+<h3 class="title"
+   ><a href="http://www.xmlvm.org/contribute/cla.php"
+            >http://www.xmlvm.org/contribute/cla.php</a></h3> 
+ 
+<p>In order to clarify the intellectual property license granted with
+Contributions from any person or entity, XMLVM must
+have a Contributor License Agreement ("CLA") on file that has been
+signed by each Contributor, indicating agreement to the license terms
+below.  This license is for your protection as a Contributor as well
+as the protection of XMLVM; it does not change your rights to use
+your own Contributions for any other purpose.  If you have not already
+done so, please complete and send an original signed Agreement to</p> 
+ 
+<blockquote> 
+  Arno Puder<br/> 
+  San Francisco State University<br/> 
+  1600 Holloway Avenue<br/>
+  San Francisco, CA  94132<br/> 
+  U.S.A.<br/> 
+</blockquote> 
+ 
+<p>Scanned agreements may also be emailed in PDF form to arno@puder.org,
+    or signed electronically with the form below.</p> 
+ 
+<p> 
+Please read this document carefully before signing and keep a copy for
+your records.</p> 
+ 
+<pre> 
+ 
+   Full name: ____________________________  E-Mail: ______________________
+   
+    
+   
+   Mailing Address: ______________________  Telephone: ___________________
+   
+    
+   
+   _______________________________________  Facsimile: ___________________
+   
+    
+   
+   _______________________________________  Country:   ___________________
+   
+   
+</pre> 
+ 
+<p>You accept and agree to the following terms and conditions for Your
+present and future Contributions submitted to XMLVM.  Except for the
+license granted herein to XMLVM and recipients of software
+distributed by XMLVM, You reserve all right, title, and interest in
+and to Your Contributions.</p> 
+ 
+<ol> 
+ 
+<li><p>Definitions.  "You" (or "Your") shall mean the copyright owner
+or legal entity authorized by the copyright owner that is making this
+Agreement with XMLVM.  For legal entities, the entity making a
+Contribution and all other entities that control, are controlled by,
+or are under common control with that entity are considered to be a
+single Contributor.  For the purposes of this definition, "control"
+means (i) the power, direct or indirect, to cause the direction or
+management of such entity, whether by contract or otherwise, or (ii)
+ownership of fifty percent (50%) or more of the outstanding shares, or
+(iii) beneficial ownership of such entity.  "Contribution" shall mean
+any original work of authorship, including any modifications or
+additions to an existing work, that is intentionally submitted by You
+to XMLVM for inclusion in, or documentation of, any of the products
+owned or managed by XMLVM (the "Work").  For the purposes of this
+definition, "submitted" means any form of electronic, verbal, or
+written communication sent to XMLVM or its representatives, including
+but not limited to communication on electronic mailing lists, source
+code control systems, and issue tracking systems that are managed by,
+or on behalf of, XMLVM for the purpose of discussing and improving
+the Work, but excluding communication that is conspicuously marked or
+otherwise designated in writing by You as "Not a
+Contribution."</p></li> 
+ 
+ 
+<li><p>Grant of Copyright License.  Subject to the terms and
+conditions of this Agreement, You hereby grant to XMLVM and to
+recipients of software distributed by XMLVM a perpetual, worldwide,
+non-exclusive, no-charge, royalty-free, irrevocable copyright license
+to reproduce, prepare derivative works of, publicly display, publicly
+perform, sublicense, and distribute Your Contributions and such
+derivative works.</p></li> 
+ 
+<li><p>Grant of Patent License.  Subject to the terms and conditions
+of this Agreement, You hereby grant to XMLVM and to recipients of
+software distributed by XMLVM a perpetual, worldwide, non-exclusive,
+no-charge, royalty-free, irrevocable (except as stated in this
+section) patent license to make, have made, use, offer to sell, sell,
+import, and otherwise transfer the Work, where such license applies
+only to those patent claims licensable by You that are necessarily
+infringed by Your Contribution(s) alone or by combination of Your
+Contribution(s) with the Work to which such Contribution(s) was
+submitted.  You furthermore agree to notify XMLVM of any patents that
+you know or come to know are likely infringed by the Contribution
+and/or are not licensable by You.  If any entity institutes patent
+litigation against You or any other entity (including a cross-claim or
+counterclaim in a lawsuit) alleging that your Contribution, or the
+Work to which you have contributed, constitutes direct or contributory
+patent infringement, then any patent licenses granted to that entity
+under this Agreement for that Contribution or Work shall terminate as
+of the date such litigation is filed.</p></li> 
+ 
+<li><p>You represent that you are legally entitled to grant the above
+license.  If your employer(s) has rights to intellectual property that
+you create that includes your Contributions, you represent that you
+have received permission to make Contributions on behalf of that
+employer, that your employer has waived such rights for your
+Contributions to XMLVM, or that your employer has executed a separate
+Corporate CLA with XMLVM.</p></li> 
+ 
+<li><p>You represent that each of Your Contributions is Your original
+creation (see section 7 for submissions on behalf of others).  You
+represent that Your Contribution submissions include complete details
+of any third-party license or other restriction (including, but not
+limited to, related patents and trademarks) of which you are
+personally aware and which are associated with any part of Your
+Contributions.</p></li> 
+ 
+<li><p>You are not expected to provide support for Your Contributions,
+except to the extent You desire to provide support.  You may provide
+support for free, for a fee, or not at all.  Unless required by
+applicable law or agreed to in writing, You provide Your Contributions
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied, including, without limitation, any
+warranties or conditions of TITLE, NON- INFRINGEMENT, MERCHANTABILITY,
+or FITNESS FOR A PARTICULAR PURPOSE.</p></li> 
+ 
+<li><p>Should You wish to submit work that is not Your original
+creation, You may submit it to XMLVM separately from any
+Contribution, identifying the complete details of its source and of
+any license or other restriction (including, but not limited to,
+related patents, trademarks, and license agreements) of which you are
+personally aware, and conspicuously marking the work as "Submitted on
+behalf of a third-party: [named here]".</p></li> 
+ 
+<li><p>You agree to notify XMLVM of any facts or circumstances of
+which you become aware that would make these representations
+inaccurate in any respect.</p></li> 
+ 
+</ol> 
+ 
+ 
+<pre> 
+ 
+ 
+Please sign: __________________________________ Date: ________________
+</pre> 
+ 
+<h2>Sign Electronically</h2> 
+<p>Alternatively to sending by postal mail, email, or fax, you may also choose to sign the form electronically:</p> 
+ 
+<form action="http://www.xmlvm.org/contribute/cla/submit.php" method="POST"> 
+ 
+<table id="cla_form"> 
+<tr valign="top"> 
+<td align="right">Full name:</td> 
+<td><input type="text" name="single:0" /></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">E-Mail:</td> 
+<td><input type="text" name="single:1" /></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">Mailing Address:</td> 
+<td><textarea class="ss-q-long" name="single:2" rows="4" cols="40"></textarea></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">Country:</td> 
+<td><input type="text" name="single:3" /></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">Telephone Number:</td> 
+<td><input type="text" name="single:5" /></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">Fax Number:</td> 
+<td><input type="text" name="single:4" /> <em>(optional)</em></td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right">Electronic Signature:</td> 
+<td><input type="text" name="single:6" /> Type "<b>I AGREE</b>" to accept the terms above.</td> 
+</tr> 
+ 
+<tr valign="top"> 
+<td align="right"></td> 
+<td><input type="submit" value="Submit" /></td> 
+</tr> 
+ 
+</table> 
+ 
+</form>
+<?php
+  include("../footer.html");
+?>
---
+++ doc/homepage/contribute/index.php
@@ -0,0 +1,33 @@
+<?php
+  include("../header.html");
+  include("../content_top.html");
+?>
+<h1>Contribute</h1>
+
+<h2 class="title">Making XMLVM better</h2> 
+
+This page is for all developer that are interested in contributing to the XMLVM project. We will explain how you can download the source code, help you understand the current code structure, tell you about our coding guidelines and show you what you need to do if you want to contribute a patch.
+
+<h3>Licensing</h3>
+XMLVM is licensed under the <a href="http://www.gnu.org/licenses/gpl-2.0.txt">GPL v2</a>
+
+<h3>Getting and compiling the source</h3>
+Our source code is currently hosted in our Subversion repository at <a href="http://www.sourceforge.net/projects/xmlvm">Sourceforge</a>. In order to check out the code, you need a <a href="http://subversion.tigris.org/">Subversion</a> client. As the team works with <a href="http://www.eclipse.org">Eclipse</a>, our recommendation is to use <a href="http://subclipse.tigris.org/">Subclipse</a>, which is a free Eclipse plugin that allows you to easily checkout and update your code. We would recommend you to install this plugin as well, if you are planning to work with Eclipse.
+
+The root for checking out the code is:
+
+<pre>http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm</pre>
+
+If you want to checkout the code via the Subversion <pre>svn</pre> cient, use the following command:
+
+<pre>svn co http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm xmlvm</pre>
+
+<h3>Source code structure</h3>
+<h3>Coding guidelines</h3>
+<h3>Submitting a patch</h3>
+<h4>Contributors license agreement</h4>
+<h4>Code reviews</h4>
+
+<?php
+  include("../footer.html");
+?>
