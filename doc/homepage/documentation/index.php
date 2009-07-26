bf00c42e406521618f2bad65b5fce451997eabe0
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-07-26 13:38:47 +0000

    Updates to homepage: - New "Documentation" area - Optimized images for faster loading - Added manual.pdf
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@483 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

---
+++ doc/homepage/documentation/index.php
@@ -0,0 +1,25 @@
+<?php
+  include("../header.html");
+  include("../content_top.html");
+?>
+<h1>Documentation</h1>
+
+<h2>Usage Manual</h2>
+
+Please the see <a href="manual.pdf">manual.pdf</a> for usage information. We are working on a more detailed user manual
+that will also give you a step by step howto.
+
+<h2>Making XMLVM better</h2>
+
+<p>
+If you are interested in contributing to the XMLVM project, we have a special page for you.
+
+<b><a href="../contribute">Contribute: Making XMLVM better</a></b>
+
+We are constantly trying to improve it. Let us know if something is missing or deserves more
+attention.
+</p>
+
+<?php
+  include("../footer.html");
+?>
