4abc671fee606ede90bc8786d0c624bbf7929a1d
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-09-21 12:20:30 +0000

    Objects which do not support NSCopying get wrapped into an NSValue object.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@509 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2objc/compat-lib/objc/java_util_Map.m
+++ src/xmlvm2objc/compat-lib/objc/java_util_Map.m
@@ -8,14 +8,16 @@
 - (java_lang_Object*) put___java_lang_Object_java_lang_Object :(java_lang_Object*) key
                                                               :(java_lang_Object*) value
 {
-	[((NSMutableDictionary*) self) setObject: value forKey: key];
+	id k = [key conformsToProtocol: @protocol(NSCopying)] ? key : [NSValue valueWithPointer: key];
+	[((NSMutableDictionary*) self) setObject: value forKey: k];
 	[value retain];
 	return value;
 }
 
 - (java_lang_Object*) get___java_lang_Object :(java_lang_Object*) key
 {
-	id value = [self objectForKey: key];
+	id k = [key conformsToProtocol: @protocol(NSCopying)] ? key : [NSValue valueWithPointer: key];
+	id value = [self objectForKey: k];
 	if (value == nil)
 		return [NSNull null];
 	[value retain];
@@ -24,8 +26,16 @@
 
 - (java_util_Set*) keySet
 {
-	NSArray* keyArray = [self allKeys];
-	NSSet* keys = [NSSet setWithArray: keyArray];
+	NSEnumerator* e = [[self allKeys] objectEnumerator];
+	NSMutableArray* ma = [[NSMutableArray alloc] init];
+	id v;
+	
+	while (v = [e nextObject]) {
+		id k = [v isKindOfClass: [NSValue class]] ? [v pointerValue] : v;
+		[ma addObject: k];
+	}
+	
+	NSSet* keys = [NSSet setWithArray: ma];
 	[keys retain];
 	return keys;
 }
