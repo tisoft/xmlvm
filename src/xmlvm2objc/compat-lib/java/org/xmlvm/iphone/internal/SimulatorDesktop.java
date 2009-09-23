d1936848d5b1eeec87d10633c82366e16873d9aa
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-09-23 19:19:49 +0000

    Ignored LWJGL errors upon terminating the simulator (i.e. caused by missing native LWJGL libraries)
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@516 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/internal/SimulatorDesktop.java
+++ src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/internal/SimulatorDesktop.java
@@ -1,4 +1,3 @@
-
 package org.xmlvm.iphone.internal;
 
 import java.awt.Color;
@@ -9,38 +8,40 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-
-
-public class SimulatorDesktop
-{
+public class SimulatorDesktop {
 
     private JFrame    window;
     private Container contentPane;
 
-
-
-    public SimulatorDesktop()
-    {
+    public SimulatorDesktop() {
         Simulator.initialized = true;
         MicroHTTPServer.start();
         window = new JFrame("iPhone Simulator");
         window.addWindowListener(new WindowAdapter() {
 
-			@Override
-			public void windowClosed(WindowEvent e) {
-				if (org.lwjgl.opengl.Display.isCreated()) {
-					org.lwjgl.opengl.Display.destroy();
-				}
-				System.exit(0);
-			}
+            @Override
+            public void windowClosed(WindowEvent e) {
+                try {
+                    if (org.lwjgl.opengl.Display.isCreated()) {
+                        org.lwjgl.opengl.Display.destroy();
+                    }
+                } catch (Throwable t) {
+                    // Ignore
+                }
+                System.exit(0);
+            }
 
-			@Override
-			public void windowClosing(WindowEvent e) {
-				if (org.lwjgl.opengl.Display.isCreated()) {
-					org.lwjgl.opengl.Display.destroy();
-				}
-				System.exit(0);
-			}
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    if (org.lwjgl.opengl.Display.isCreated()) {
+                        org.lwjgl.opengl.Display.destroy();
+                    }
+                } catch (Throwable t) {
+                    // Ignore
+                }
+                System.exit(0);
+            }
         });
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setSize(580, 750);
