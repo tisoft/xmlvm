249f57073eff14c49e6349639ab3aee8a7f8a0a4
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-10-15 01:20:04 +0000

    First few lines of palm sdk sanity checking.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@554 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm/org/xmlvm/proc/out/Android2PalmPreOutputProcess.java
+++ src/xmlvm/org/xmlvm/proc/out/Android2PalmPreOutputProcess.java
@@ -20,9 +20,14 @@
 
 package org.xmlvm.proc.out;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.xmlvm.Log;
 import org.xmlvm.main.Arguments;
 
 /**
@@ -33,7 +38,11 @@ import org.xmlvm.main.Arguments;
  */
 public class Android2PalmPreOutputProcess extends OutputProcess<QooxdooOutputProcess> {
 
-    List<OutputFile> outputFiles = new ArrayList<OutputFile>();
+    // TODO(Sascha): Make this platform-independant.
+    private static final String PALM_GENERATE_CMD = "palm-generate.bat";
+    private static final String VERSION_ARG       = "--version";
+    private static final String PALM_SDK_PATH     = System.getenv("XMLVM_PALM_SDK_PATH");
+    private List<OutputFile>    outputFiles       = new ArrayList<OutputFile>();
 
     public Android2PalmPreOutputProcess(Arguments arguments) {
         super(arguments);
@@ -48,6 +57,9 @@ public class Android2PalmPreOutputProcess extends OutputProcess<QooxdooOutputPro
 
     @Override
     public boolean process() {
+        if (!peformSanityChecks()) {
+            return false;
+        }
         // Just copy over the files from the Qooxdoo Process.
         List<QooxdooOutputProcess> preprocesses = preprocess();
         for (QooxdooOutputProcess process : preprocesses) {
@@ -59,5 +71,57 @@ public class Android2PalmPreOutputProcess extends OutputProcess<QooxdooOutputPro
     @Override
     public boolean postProcess() {
         return true;
+    }
+
+    private boolean peformSanityChecks() {
+        return arePalmToolsPresent();
+    }
+
+    /**
+     * Returns whether the Python runtime is all set.
+     */
+    private static boolean arePalmToolsPresent() {
+        Log.debug("Checking for Palm SDK Tools ...");
+
+        if (PALM_SDK_PATH.isEmpty()) {
+            Log.error("You have to set the Path to your Palm 'SDK' directory using the "
+                    + "environment variable XMLVM_PALM_SDK_PATH.");
+            return false;
+        }
+
+        File palmSdkDir = new File(PALM_SDK_PATH);
+        if (!palmSdkDir.exists() || !palmSdkDir.isDirectory()) {
+            Log.error("The Palm SDK PATH '" + PALM_SDK_PATH
+                    + "' does not exist or is not a directory.");
+            return false;
+        }
+
+        File palmSdkBinDir = new File(PALM_SDK_PATH + "/bin");
+        if (!palmSdkBinDir.exists() || !palmSdkBinDir.isDirectory()) {
+            Log.error("The Palm SDK PATH '" + PALM_SDK_PATH
+                    + "' does not contain a 'bin' directory.");
+            return false;
+        }
+
+        String line = "";
+        try {
+            Process process;
+            process = Runtime.getRuntime().exec(
+                    new String[] {
+                            palmSdkBinDir.getAbsolutePath() + File.separator + PALM_GENERATE_CMD,
+                            VERSION_ARG });
+            BufferedReader input = new BufferedReader(new InputStreamReader(process
+                    .getInputStream()));
+            line = input.readLine();
+            process.destroy();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (line != null && line.startsWith("Version")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
