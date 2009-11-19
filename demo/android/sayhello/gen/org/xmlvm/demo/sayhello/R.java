28c9a7987e37d14c35eb2daf75a39899d0063a26
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-11-19 10:25:23 +0000

    Logo view added.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@610 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- demo/android/sayhello/gen/org/xmlvm/demo/sayhello/R.java
+++ demo/android/sayhello/gen/org/xmlvm/demo/sayhello/R.java
@@ -12,11 +12,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int xmlvm_logo=0x7f020001;
     }
     public static final class id {
         public static final int helloLabel=0x7f050001;
         public static final int name=0x7f050000;
         public static final int sayHello=0x7f050002;
+        public static final int xmlvmLogo=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
