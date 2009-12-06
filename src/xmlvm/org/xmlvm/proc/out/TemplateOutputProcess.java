eeb97c7f01cda738ea33c57fa6bc8babab7dd86a
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-12-06 19:37:24 +0000

    Cleaning up...
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@672 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm/org/xmlvm/proc/out/TemplateOutputProcess.java
+++ src/xmlvm/org/xmlvm/proc/out/TemplateOutputProcess.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2004-2009 XMLVM --- An XML-based Programming Language
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ * 
+ * For more information, visit the XMLVM Home Page at http://www.xmlvm.org
+ */
 
 package org.xmlvm.proc.out;
 
@@ -11,58 +30,59 @@ import org.xmlvm.util.FileUtil;
 import org.xmlvm.util.JarUtil;
 
 /**
- * 
- * @author teras
+ * Creates a template project.
  */
-public class TemplateOutputProcess extends OutputProcess {
+public class TemplateOutputProcess extends OutputProcess<InputProcess<?>> {
 
-    private static final String     IPHONE_TEMPL_PROJNAME  = "__PROJNAME__";
-    private static final String     IPHONE_TEMPL_JAR_PATH  = "/iphone/netbeans/";
-    private static final String     IPHONE_TEMPL_FILE_PATH = "var/iphone/netbeans/";
-    private static final String[][] Files                  = { { "Main.java", "src/xmlvm/" },
-            { "build-impl.xml", "nbproject/" }, { "build.xml", "" },
-            { "genfiles.properties", "nbproject/" }, { "manifest.mf", "" },
-            { "project.properties", "nbproject/" }, { "project.xml", "nbproject/" },
-            { ".project", "" }, { ".classpath", "" }      };
-    private List<OutputFile>        result                 = new ArrayList<OutputFile>();
+	private static final String IPHONE_TEMPL_PROJNAME = "__PROJNAME__";
+	private static final String IPHONE_TEMPL_JAR_PATH = "/iphone/netbeans/";
+	private static final String IPHONE_TEMPL_FILE_PATH = "var/iphone/netbeans/";
+	private static final String[][] Files = { { "Main.java", "src/xmlvm/" },
+			{ "build-impl.xml", "nbproject/" }, { "build.xml", "" },
+			{ "genfiles.properties", "nbproject/" }, { "manifest.mf", "" },
+			{ "project.properties", "nbproject/" },
+			{ "project.xml", "nbproject/" }, { ".project", "" },
+			{ ".classpath", "" } };
+	private List<OutputFile> result = new ArrayList<OutputFile>();
 
-    public TemplateOutputProcess(Arguments arguments) {
-        super(arguments);
-        addSupportedInput(InputProcess.class);
-    }
+	public TemplateOutputProcess(Arguments arguments) {
+		super(arguments);
+		addSupportedInput(InputProcess.class);
+	}
 
-    @Override
-    public List getOutputFiles() {
-        return result;
-    }
+	@Override
+	public List<OutputFile> getOutputFiles() {
+		return result;
+	}
 
-    @Override
-    public boolean process() {
-        String projname = arguments.option_app_name();
-        String outpath = arguments.option_out() + "/";
+	@Override
+	public boolean process() {
+		String projname = arguments.option_app_name();
+		String outpath = arguments.option_out() + "/";
 
-        for (int i = 0; i < Files.length; i++) {
-            if (!addFile(Files[i][0], outpath + Files[i][1], projname))
-                return false;
-        }
+		for (int i = 0; i < Files.length; i++) {
+			if (!addFile(Files[i][0], outpath + Files[i][1], projname))
+				return false;
+		}
 
-        File jarlib = new File(JarUtil.findSelfJar());
-        ResourceOutputFile jarout = new ResourceOutputFile(jarlib.getParent(), outpath + "lib/",
-                jarlib.getName());
-        result.add(jarout);
+		File jarlib = new File(JarUtil.findSelfJar());
+		ResourceOutputFile jarout = new ResourceOutputFile(jarlib.getParent(),
+				outpath + "lib/", jarlib.getName());
+		result.add(jarout);
 
-        return true;
-    }
+		return true;
+	}
 
-    private boolean addFile(String filename, String to, String projname) {
-        OutputFile file = new OutputFile(null);
-        file.setFileName(filename);
-        file.setLocation(to);
-        if (!file.setDataFromStream(FileUtil.findStreamResource(IPHONE_TEMPL_JAR_PATH + filename,
-                IPHONE_TEMPL_FILE_PATH + filename)))
-            return false;
-        file.setData(file.getData().replace(IPHONE_TEMPL_PROJNAME, projname));
-        result.add(file);
-        return true;
-    }
+	private boolean addFile(String filename, String to, String projname) {
+		OutputFile file = new OutputFile(null);
+		file.setFileName(filename);
+		file.setLocation(to);
+		if (!file.setDataFromStream(FileUtil.findStreamResource(
+				IPHONE_TEMPL_JAR_PATH + filename, IPHONE_TEMPL_FILE_PATH
+						+ filename)))
+			return false;
+		file.setData(file.getData().replace(IPHONE_TEMPL_PROJNAME, projname));
+		result.add(file);
+		return true;
+	}
 }
