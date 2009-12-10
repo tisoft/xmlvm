48e87e9eb0d6657c8d8b488f27aa67e32ed902ba
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-12-10 20:03:29 +0000

    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@695 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- doc/homepage/redirect.php
+++ doc/homepage/redirect.php
@@ -5,6 +5,8 @@
         return "http://www.sigs-datacom.de/fachzeitschriften/javaspektrum/archiv/artikelansicht.html?tx_mwjournals_pi1[pointer]=0&tx_mwjournals_pi1[mode]=1&tx_mwjournals_pi1[showUid]=6495";
       } else if ($url == "@bluecarat") {
         return "http://www.bluecarat.eu";
+      } else if ($url == "@ixokoban") {
+        return "http://sourceforge.net/mailarchive/forum.php?thread_name=4B20C662.2070008%40puder.org&forum_name=xmlvm-users";
       }
     }
     return $url;
