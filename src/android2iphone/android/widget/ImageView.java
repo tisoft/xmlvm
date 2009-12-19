2a36d7c26e74226119c405ebadcf921cd66124cc
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-12-19 15:06:01 +0000

    Adding scale-type to ImageView.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@718 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/android2iphone/android/widget/ImageView.java
+++ src/android2iphone/android/widget/ImageView.java
@@ -38,6 +38,67 @@ import android.widget.AbsoluteLayout.LayoutParams;
 
 public class ImageView extends View {
 
+    /**
+     * Options for scaling the bounds of an image to the bounds of this view.
+     * <p>
+     * <i>(This enum is taken from Android's source code.)</i>
+     */
+    public enum ScaleType {
+        /**
+         * Scale using the image matrix when drawing. The image matrix can be
+         * set using {@link ImageView#setImageMatrix(Matrix)}. From XML, use
+         * this syntax: <code>android:scaleType="matrix"</code>.
+         */
+        MATRIX(0),
+        /**
+         * Scale the image using {@link Matrix.ScaleToFit#FILL}. From XML, use
+         * this syntax: <code>android:scaleType="fitXY"</code>.
+         */
+        FIT_XY(1),
+        /**
+         * Scale the image using {@link Matrix.ScaleToFit#START}. From XML, use
+         * this syntax: <code>android:scaleType="fitStart"</code>.
+         */
+        FIT_START(2),
+        /**
+         * Scale the image using {@link Matrix.ScaleToFit#CENTER}. From XML, use
+         * this syntax: <code>android:scaleType="fitCenter"</code>.
+         */
+        FIT_CENTER(3),
+        /**
+         * Scale the image using {@link Matrix.ScaleToFit#END}. From XML, use
+         * this syntax: <code>android:scaleType="fitEnd"</code>.
+         */
+        FIT_END(4),
+        /**
+         * Center the image in the view, but perform no scaling. From XML, use
+         * this syntax: <code>android:scaleType="center"</code>.
+         */
+        CENTER(5),
+        /**
+         * Scale the image uniformly (maintain the image's aspect ratio) so that
+         * both dimensions (width and height) of the image will be equal to or
+         * larger than the corresponding dimension of the view (minus padding).
+         * The image is then centered in the view. From XML, use this syntax:
+         * <code>android:scaleType="centerCrop"</code>.
+         */
+        CENTER_CROP(6),
+        /**
+         * Scale the image uniformly (maintain the image's aspect ratio) so that
+         * both dimensions (width and height) of the image will be equal to or
+         * less than the corresponding dimension of the view (minus padding).
+         * The image is then centered in the view. From XML, use this syntax:
+         * <code>android:scaleType="centerInside"</code>.
+         */
+        CENTER_INSIDE(7);
+
+        ScaleType(int ni) {
+            nativeInt = ni;
+        }
+
+        final int nativeInt;
+    }
+
     protected Drawable drawable;
 
     public ImageView(Context c) {
@@ -87,6 +148,10 @@ public class ImageView extends View {
         int y = l instanceof AbsoluteLayout.LayoutParams ? ((AbsoluteLayout.LayoutParams) l).y : 0;
 
         getUIImageView().setFrame(new CGRect(x, y, width, height));
+    }
+
+    public void setScaleType(ScaleType type) {
+        Assert.NOT_IMPLEMENTED();
     }
 
     @Override
