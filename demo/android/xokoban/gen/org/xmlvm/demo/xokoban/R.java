36e52352ddf666c85ab7871b2dc9a38b4f3a24cc
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2009-12-19 22:27:33 +0000

    Updating R class to contain the HD versions of the graphics.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@725 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- demo/android/xokoban/gen/org/xmlvm/demo/xokoban/R.java
+++ demo/android/xokoban/gen/org/xmlvm/demo/xokoban/R.java
@@ -12,21 +12,26 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ball_20=0x7f020001;
-        public static final int ball_30=0x7f020002;
-        public static final int floor_20=0x7f020003;
-        public static final int floor_30=0x7f020004;
-        public static final int goal_20=0x7f020005;
-        public static final int goal_30=0x7f020006;
-        public static final int icon=0x7f020007;
-        public static final int info=0x7f020008;
-        public static final int levels=0x7f020009;
-        public static final int man_20=0x7f02000a;
-        public static final int man_30=0x7f02000b;
-        public static final int splash=0x7f02000c;
-        public static final int splash_info=0x7f02000d;
-        public static final int wall_20=0x7f02000e;
-        public static final int wall_30=0x7f02000f;
+        public static final int ball_100=0x7f020001;
+        public static final int ball_20=0x7f020002;
+        public static final int ball_30=0x7f020003;
+        public static final int floor_100=0x7f020004;
+        public static final int floor_20=0x7f020005;
+        public static final int floor_30=0x7f020006;
+        public static final int goal_100=0x7f020007;
+        public static final int goal_20=0x7f020008;
+        public static final int goal_30=0x7f020009;
+        public static final int icon=0x7f02000a;
+        public static final int info=0x7f02000b;
+        public static final int levels=0x7f02000c;
+        public static final int man_100=0x7f02000d;
+        public static final int man_20=0x7f02000e;
+        public static final int man_30=0x7f02000f;
+        public static final int splash=0x7f020010;
+        public static final int splash_info=0x7f020011;
+        public static final int wall_100=0x7f020012;
+        public static final int wall_20=0x7f020013;
+        public static final int wall_30=0x7f020014;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
