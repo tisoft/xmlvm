14793814e95448d90c7e05841c0c7f5a07182298
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-02-22 09:56:48 +0000

    Added proper Copyright headers.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@936 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2js/org/gmap/InternalEvent.js
+++ src/xmlvm2js/org/gmap/InternalEvent.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2004-2010 XMLVM --- An XML-based Programming Language
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ * 
+ * For more information, visit the XMLVM Home Page at http://www.xmlvm.org
+ */
+
 // Internal Events are stored in an queue of GMap2. This is necessary, as it is
 // possible to call methods like addOverlay on the Map, while the map is not
 // yet added to the DOM (because it is not yet set visible). It doesn't seem to
