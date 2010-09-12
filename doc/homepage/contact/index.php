951a120101e5a8818fe160a11993c310cd5e78b6
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-09-12 10:20:52 +0000

    Updated list of contributors.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1154 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- doc/homepage/contact/index.php
+++ doc/homepage/contact/index.php
@@ -13,9 +13,13 @@
 
 <h2>Contributors:</h2>
 <ul>
-  <li><div id="leigh">Leigh Jin</div></li>
-  <li><div id="jessica">Jessica Lee</div></li>  
-	<li><div id="joshua">Joshua Melcon (Developer)</div></li>
+  <li>Kevin Glass</li>
+  <li>Panayotis Katsaloulis</li>
+  <li>Gergely Kis</li>
+  <li>Jessica Lee</li>
+  <li>Joshua Melcon</li>
+  <li>Paul Poley</li>
+  <li>Alexander Seewald</li>
 </ul>
 
 
@@ -33,24 +37,9 @@
   frusty += '&#64;</b>xmlvm&#46';
   frusty += ';&#111;rg&gt; (Developer)';
   
-  var leigh = 'Leigh Jin &lt;leigh<b>';
-  leigh += '&#64;</b>xmlvm&#46';
-  leigh += ';&#111;rg&gt; (Business Development)';
-  
-  var jessica = 'Jessica Lee &lt;jeslucas<b>';
-  jessica += '&#64;</b>hotmail&#46';
-  jessica += 'com&gt; (Developer)';
-  
-  var joshua = 'Joshua Melcon &lt;xmlvm<b>';
-  joshua += '&#64;</b>melcon&#46';
-  joshua += 'us&gt; (Developer)';
-  
   document.getElementById('arno').innerHTML = arno;
   document.getElementById('sascha').innerHTML = sascha;
-  document.getElementById('leigh').innerHTML = leigh;
   document.getElementById('frusty').innerHTML = frusty;
-  document.getElementById('jessica').innerHTML = jessica;
-  document.getElementById('joshua').innerHTML = joshua;
 
 //-->
 </script>
