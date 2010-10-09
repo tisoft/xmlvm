116b503c0c2600b5413232b17c2660d8366d2fd0
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-10-09 00:26:10 +0000

    Add some needed methods.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1163 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2objc/compat-lib/objc/java_util_Locale.h
+++ src/xmlvm2objc/compat-lib/objc/java_util_Locale.h
@@ -24,12 +24,22 @@
 // java.util.Locale
 //----------------------------------------------------------------------------
 @interface java_util_Locale : java_lang_Object {
-	java_lang_String *prefLang;
+	java_lang_String *language;
+	NSUInteger        languageHash;
+	java_lang_String* country;
+	NSUInteger        countryHash;
 }
 
-- (id) init:(java_lang_String*)prefLang;
++ (void) initialize;
+- (id) init;
+- (void) __init_java_lang_String___java_lang_String: (java_lang_String*) theLanguage;
+- (void) __init_java_lang_String___java_lang_String_java_lang_String:(java_lang_String*) theLanguage
+                                                                    :(java_lang_String*) theCountry;
 - (void) dealloc;
 + (java_util_Locale*) getDefault__;
+- (int) equals___java_lang_Object:(java_lang_Object*) obj;
 - (java_lang_String*) toString__;
-
+- (java_lang_String*) getLanguage__;
+- (java_lang_String*) getCountry__;
+	
 @end
--- src/xmlvm2objc/compat-lib/objc/java_util_Locale.m
+++ src/xmlvm2objc/compat-lib/objc/java_util_Locale.m
@@ -25,17 +25,43 @@
 //----------------------------------------------------------------------------
 @implementation java_util_Locale
 
-- (id) init :(java_lang_String*)prefLang
+static Class localeClass;
+
++ (void) initialize
+{
+	localeClass = [java_util_Locale class];
+}	
+
+- (id) init
 {
 	if (self = [super init]) {
-  	self->prefLang=[prefLang retain];
+		self->language = JAVA_NULL;
+		self->languageHash = 0;
+		self->country = JAVA_NULL;
+		self->countryHash = 0;
 	}
 	return self;
 }
 
-- (void) dealloc;
+- (void) __init_java_lang_String___java_lang_String: (java_lang_String*) theLanguage
 {
-	[prefLang release];
+	language = [theLanguage retain];
+	languageHash = [theLanguage hash];
+}
+
+- (void) __init_java_lang_String___java_lang_String_java_lang_String:(java_lang_String*) theLanguage
+                                                                    :(java_lang_String*) theCountry
+{
+	language = [theLanguage retain];
+	languageHash = [theLanguage hash];
+	country = [theCountry retain];
+	countryHash = [theCountry hash];
+}
+
+- (void) dealloc
+{
+	[language release];
+	[country release];
 	[super dealloc];
 }
 
@@ -45,13 +71,40 @@
 	NSArray* languages = [defs objectForKey:@"AppleLanguages"];
 	NSString* preferredLang = [languages objectAtIndex:0];
 
-	java_util_Locale *l = [[java_util_Locale alloc] init:preferredLang];
+	java_util_Locale *l = [[java_util_Locale alloc] init];
+	[l __init_java_lang_String___java_lang_String:preferredLang];
 	return l;
+}
+
+- (int) equals___java_lang_Object:(java_lang_Object*) obj
+{
+	if (![obj isKindOfClass:localeClass]) {
+		return 0;
+	}
+	java_util_Locale* other = (java_util_Locale*) obj;
+	if ((countryHash != other->countryHash) || (languageHash != other->languageHash)) {
+		return 0;
+	}
+	// Hash values match. Now make sure that the strings are actually equal
+	if ([language isEqual:other->language] && [country isEqual:other->country]) {
+		return 1;
+	}
+	return 0;
 }
 
 - (java_lang_String*) toString__
 {
-  return [prefLang retain];
+  return [language retain];
+}
+
+- (java_lang_String*) getLanguage__
+{
+	return [language retain];
+}
+
+- (java_lang_String*) getCountry__
+{
+	return [country retain];
 }
 
 @end
