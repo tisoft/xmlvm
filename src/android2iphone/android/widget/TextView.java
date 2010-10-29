3cb0c99c03bd65cc7ef203f1f21e9375b840ef75
wkorn <wkorn@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-10-29 13:16:38 +0000

    Imports organized.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1193 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/android2iphone/android/widget/TextView.java
+++ src/android2iphone/android/widget/TextView.java
@@ -42,7 +42,6 @@ import android.internal.Dimension;
 import android.internal.XMLVMTheme;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
