1e7e8d1cf6ee21ddf2236ac66a134cd8efdd23fe
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-12-09 01:09:47 +0000

    - Show spinner while page is loading. - Show error message when page cannot be loaded.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1273 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/android2iphone/android/webkit/WebView.java
+++ src/android2iphone/android/webkit/WebView.java
@@ -29,12 +29,25 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import java.util.Set;
+
+import org.xmlvm.iphone.CGRect;
+import org.xmlvm.iphone.NSError;
+import org.xmlvm.iphone.UIActivityIndicatorView;
+import org.xmlvm.iphone.UIActivityIndicatorViewStyle;
 import org.xmlvm.iphone.UIColor;
 import org.xmlvm.iphone.UIEvent;
 import org.xmlvm.iphone.UITouch;
 import org.xmlvm.iphone.UIView;
+import org.xmlvm.iphone.UIWebViewDelegate;
 
 public class WebView extends ViewGroup {
+
+    private static final String errorMessage = "<html><body style=\"height: 100%;\"><table style=\"width: 100%; height: 100%; text-align: center;\"><tr><td>"
+                                                     + "Unable to load page!"
+                                                     + "</td></tr></table></body></html>";
+
+    private UIWebViewDelegate   delegate;
+
 
     public WebView(Context c) {
         super(c);
@@ -68,7 +81,45 @@ public class WebView extends ViewGroup {
                 xmlvmTouchesEvent(MotionEvent.ACTION_UP, touches, event);
             }
         };
+        delegate = new UIWebViewDelegate() {
+
+            private UIActivityIndicatorView spinner;
+
+
+            @Override
+            public void didFailLoadWithError(UIWebView webView, NSError error) {
+                removeSpinner();
+                webView.loadHTMLString(errorMessage, null);
+            }
+
+            @Override
+            public void webViewDidFinishLoad(UIWebView webView) {
+                removeSpinner();
+            }
+
+            @Override
+            public void webViewDidStartLoad(UIWebView webView) {
+                spinner = new UIActivityIndicatorView();
+                spinner.setActivityIndicatorViewStyle(UIActivityIndicatorViewStyle.Gray);
+                CGRect webViewRect = webView.getBounds();
+                CGRect spinnerRect = spinner.getBounds();
+                spinnerRect.origin.x = (webViewRect.size.width - spinnerRect.size.width) / 2;
+                spinnerRect.origin.y = (webViewRect.size.height - spinnerRect.size.height) / 2;
+                spinner.setFrame(spinnerRect);
+                webView.addSubview(spinner);
+                spinner.startAnimating();
+            }
+
+            private void removeSpinner() {
+                if (spinner != null) {
+                    spinner.stopAnimating();
+                    spinner.removeFromSuperview();
+                    spinner = null;
+                }
+            }
+        };
         view.setBackgroundColor(UIColor.whiteColor);
+        view.setDelegate(delegate);
         return view;
     }
 
