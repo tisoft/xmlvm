b4001dea705e0c46adc3aaf20f00da22783a4b95
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2010-12-28 20:55:31 +0000

    Adds a nicely readable error message, if XMLVM didn't get enough memory to run.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1338 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm/org/xmlvm/Main.java
+++ src/xmlvm/org/xmlvm/Main.java
@@ -49,23 +49,28 @@ public class Main {
         // Instantiate the processor.
         XmlvmProcessor processor = new XmlvmProcessor(arguments);
 
-        // 1) Processing.
-        if (processor.process()) {
-            Log.debug("Processing finished successfully.");
-            // 2) Writing files.
-            if (processor.writeOutputFiles()) {
-                Log.debug("Files written successfully.");
-                // 3) Post-Processing.
-                if (processor.postProcess()) {
-                    Log.debug("Post-Processing successful.");
+        try {
+            // 1) Processing.
+            if (processor.process()) {
+                Log.debug("Processing finished successfully.");
+                // 2) Writing files.
+                if (processor.writeOutputFiles()) {
+                    Log.debug("Files written successfully.");
+                    // 3) Post-Processing.
+                    if (processor.postProcess()) {
+                        Log.debug("Post-Processing successful.");
+                    } else {
+                        Log.error("Something went wrong during post-processing.");
+                    }
                 } else {
-                    Log.error("Something went wrong during post-processing.");
+                    Log.error("Something went wrong while writing files.");
                 }
             } else {
-                Log.error("Something went wrong while writing files.");
+                Log.error("Something went wrong during processing.");
             }
-        } else {
-            Log.error("Something went wrong during processing.");
+        } catch (OutOfMemoryError e) {
+            Log.error("Oh no, XMLVM needs more memory. Try running with -Xmx1G.");
+            return;
         }
 
         // If timer was configured, print result.
