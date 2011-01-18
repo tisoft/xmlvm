a7bedfb25181b33b0bf8f8be4f516d8868174628
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2011-01-18 22:11:05 +0000

    - Added license exception - Removed download documentation section
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1439 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- doc/homepage/download/index.php
+++ doc/homepage/download/index.php
@@ -21,7 +21,20 @@ contain all necessary libraries required to compile and use XMLVM.
 <p>XMLVM is released under the
   <a href="http://www.gnu.org/licenses/lgpl-2.1.html" target="_blank">
     GNU Lesser General Public License, version 2.1.
-  </a>
+  </a><br/>
+  In addition we grand you the following exception:<br/>
+  <div style="margin-left:30px"><pre>As a special exception to the GNU Lesser General Public License, you
+may link, statically or dynamically, a "work that uses the Library"
+with a publicly distributed version of the Library to produce an
+executable file containing portions of the Library, and distribute that
+executable file under terms of your choice, without any of the additional
+requirements listed in clause 6 of the GNU Lesser General Public License.
+By "a publicly distributed version of the Library", we mean either the
+unmodified Library as distributed by the authors, or a modified version
+of the Library that is distributed under the conditions defined in clause
+3 of the GNU Lesser General Public License.  This exception does not
+however invalidate any other reasons why the executable file might be
+covered by the GNU Lesser General Public License.</pre></div>
 </p>
 
 <h2>Stable Releases</h2>
@@ -29,13 +42,6 @@ contain all necessary libraries required to compile and use XMLVM.
 Since XMLVM is currently in an early development phase we do not provide a stable release.
 All releases currently offered are experimental development releases that are not to be used
 in a production environment.
-</p>
-
-<h2>Documentation</h2>
-<p>
-A first version of the <a href="http://xmlvm.svn.sourceforge.net/viewvc/xmlvm/trunk/xmlvm/doc/manual/manual.pdf">XMLVM
-documentation</a> is available. The current version covers the installation of XMLVM as well as the iPhone/Android
-portion of the toolchain.
 </p>
 
 <?php
