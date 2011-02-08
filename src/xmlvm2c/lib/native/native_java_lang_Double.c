d8d76d6fbc783ffbaf256dafbf559a03de0aafd4
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2011-02-08 08:14:26 +0000

    Fixes.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1491 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm2c/lib/native/native_java_lang_Double.c
+++ src/xmlvm2c/lib/native/native_java_lang_Double.c
@@ -9,21 +9,39 @@
 JAVA_LONG java_lang_Double_doubleToLongBits___double(JAVA_DOUBLE n1)
 {
     //XMLVM_BEGIN_NATIVE[java_lang_Double_doubleToLongBits___double]
-    return (JAVA_LONG) n1;
+    union {
+        JAVA_DOUBLE d;
+        JAVA_LONG   l;
+    } u;
+    
+    u.d = n1;
+    return u.l;
     //XMLVM_END_NATIVE
 }
 
 JAVA_LONG java_lang_Double_doubleToRawLongBits___double(JAVA_DOUBLE n1)
 {
     //XMLVM_BEGIN_NATIVE[java_lang_Double_doubleToRawLongBits___double]
-    return (JAVA_LONG) n1;
+    union {
+        JAVA_DOUBLE d;
+        JAVA_LONG   l;
+    } u;
+    
+    u.d = n1;
+    return u.l;
     //XMLVM_END_NATIVE
 }
 
 JAVA_DOUBLE java_lang_Double_longBitsToDouble___long(JAVA_LONG n1)
 {
     //XMLVM_BEGIN_NATIVE[java_lang_Double_longBitsToDouble___long]
-	return (JAVA_DOUBLE) n1;
+    union {
+        JAVA_DOUBLE d;
+        JAVA_LONG   l;
+    } u;
+    
+    u.l = n1;
+    return u.d;
     //XMLVM_END_NATIVE
 }
 
