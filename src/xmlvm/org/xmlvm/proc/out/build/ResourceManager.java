eb395f181e520a9b6328eb8b7a38148e5cdc8816
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2011-06-05 18:24:36 +0000

    - Fixes a problem with absolute references not being added to the build-time Xcode script - Fixes style issues with missing braces.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1707 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- src/xmlvm/org/xmlvm/proc/out/build/ResourceManager.java
+++ src/xmlvm/org/xmlvm/proc/out/build/ResourceManager.java
@@ -70,38 +70,52 @@ public class ResourceManager {
 
     private static Set<String> getFilteredList(String projectpath, Set<String> reslist,
             String resourceRoot, boolean accept_sources) {
-        
+
         projectpath = getAbsoluteProjectLocation(projectpath);
         Set<String> list = new HashSet<String>();
-        if (resourceRoot == null)
+        if (resourceRoot == null) {
             resourceRoot = System.getProperty("user.dir");
+        }
         for (String resource : reslist) {
+            // First treat the resource as a relative resource and see if it
+            // exists.
             File resourceFile = new File(resourceRoot, resource);
-            if (resourceFile.exists())
-                if (resourceFile.isFile() || (!resource.endsWith(File.separator))) 
+            if (!resourceFile.exists()) {
+                // If treating the resource as a relative resource doesn't
+                // exist, try it as an absolute resource.
+                resourceFile = new File(resource);
+            }
+            if (resourceFile.exists()) {
+                if (resourceFile.isFile() || (!resource.endsWith(File.separator))) {
                     // Is a file
                     addFilteredResource(projectpath, list, resource, accept_sources);
-                else // Is a directory
-                if (resource.endsWith("/")) { // We only need the contents of
-                    // this directory
-                    File[] entries = resourceFile.listFiles();
-                    if (entries != null) { // This directory is not empty
-                        for (File entry : entries) {
-                            String name = resource + entry.getName(); // Get the
-                            // filename
-                            if (entry.isFile()) // If it is a file, only add
-                                // non-source files
-                                addFilteredResource(projectpath, list, name, accept_sources);
-                            else { // If it is a directory, keep a verbatim copy
-                                if (!accept_sources)
-                                    addResource(projectpath, list, name);
+                } else {
+                    // Is a directory
+                    if (resource.endsWith("/")) {
+                        // We only need the contents of this directory
+                        File[] entries = resourceFile.listFiles();
+                        // This directory is not empty
+                        if (entries != null) {
+                            for (File entry : entries) {
+                                // Get the filename.
+                                String name = resource + entry.getName();
+                                // If it is a file, only add non-source files.
+                                if (entry.isFile()) {
+                                    addFilteredResource(projectpath, list, name, accept_sources);
+                                } else {
+                                    // If it is a directory, keep a verbatim
+                                    // copy.
+                                    if (!accept_sources)
+                                        addResource(projectpath, list, name);
+                                }
                             }
                         }
+                    } else { // We want a verbatim copy of this directory
+                        if (!accept_sources)
+                            addResource(projectpath, list, resource);
                     }
-                } else { // We want a verbatim copy of this directory
-                    if (!accept_sources)
-                        addResource(projectpath, list, resource);
                 }
+            }
         }
         return list;
     }
@@ -119,8 +133,8 @@ public class ResourceManager {
      *            sources are not accepted, then not any source file will be
      *            added.
      */
-    private static void addFilteredResource(String projectpath, Set<String> list,
-            String resource, boolean accept_source) {
+    private static void addFilteredResource(String projectpath, Set<String> list, String resource,
+            boolean accept_source) {
         String resourceLC = resource.toLowerCase();
         boolean is_source = resourceLC.endsWith(".c") || resourceLC.endsWith(".h")
                 || resourceLC.endsWith(".m") || resourceLC.endsWith(".cpp")
@@ -128,13 +142,18 @@ public class ResourceManager {
         if (accept_source == is_source)
             addResource(projectpath, list, resource);
     }
-    
+
     /**
-     * Addd a resource file in the list, and check if the file is inside the project
-     * path or not. If it is, a relative filename is added, or else an absolute filename.
-     * @param projectpath The path of the project
-     * @param list The list of resources
-     * @param resource The resource filename
+     * Addd a resource file in the list, and check if the file is inside the
+     * project path or not. If it is, a relative filename is added, or else an
+     * absolute filename.
+     * 
+     * @param projectpath
+     *            The path of the project
+     * @param list
+     *            The list of resources
+     * @param resource
+     *            The resource filename
      */
     private static void addResource(String projectpath, Set<String> list, String resource) {
         try {
@@ -149,19 +168,21 @@ public class ResourceManager {
     }
 
     /**
-     * Get the absolute location of the project, as a canonical path name.
-     * In the special case that this project filename ends with "build/.xcode"
-     * (which is the output location of XMLVM projects as created by the
-     * ant tasks), then the project is actually two directory levels up.
-     * @param projectpath The given path of the project
+     * Get the absolute location of the project, as a canonical path name. In
+     * the special case that this project filename ends with "build/.xcode"
+     * (which is the output location of XMLVM projects as created by the ant
+     * tasks), then the project is actually two directory levels up.
+     * 
+     * @param projectpath
+     *            The given path of the project
      * @return The actual path of the project
      */
     private static String getAbsoluteProjectLocation(String projectpath) {
         try {
             projectpath = new File(projectpath).getCanonicalPath();
             if (projectpath.endsWith("/build/.xcode")) {
-                projectpath = projectpath.substring(0, projectpath.length() 
-                        - "/build/.xcode".length());
+                projectpath = projectpath.substring(0,
+                        projectpath.length() - "/build/.xcode".length());
             }
         } catch (IOException ex) {
             Log.error(ex.getMessage());
