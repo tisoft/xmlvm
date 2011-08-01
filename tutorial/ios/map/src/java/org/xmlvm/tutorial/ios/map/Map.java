0906ff84bb49a00e807950e7b97938fe645ddc41
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2011-08-01 21:19:17 +0000

    Add iOS tutorial for GPS and map API (initial version submitted by Andrew Wen, issue 138001)
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@1900 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

---
+++ tutorial/ios/map/src/java/org/xmlvm/tutorial/ios/map/Map.java
@@ -0,0 +1,73 @@
+/* Copyright (c) 2002-2011 by XMLVM.org
+ *
+ * Project Info:  http://www.xmlvm.org
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+package org.xmlvm.tutorial.ios.map;
+
+import org.xmlvm.iphone.CGRect;
+import org.xmlvm.iphone.CLLocationCoordinate2D;
+import org.xmlvm.iphone.MKMapView;
+import org.xmlvm.iphone.MKCoordinateRegion;
+import org.xmlvm.iphone.MKCoordinateSpan;
+import org.xmlvm.iphone.UIApplication;
+import org.xmlvm.iphone.UIApplicationDelegate;
+import org.xmlvm.iphone.UIScreen;
+import org.xmlvm.iphone.UIWindow;
+
+/**
+ * This application displays a map of San Francisco (the coolest place to live!)
+ * spanning the whole screen. It makes use of the MKMapView class.
+ */
+public class Map extends UIApplicationDelegate {
+
+    @Override
+    public void applicationDidFinishLaunching(UIApplication app) {
+        CGRect rect = UIScreen.mainScreen().getApplicationFrame();
+        UIWindow window = new UIWindow(rect);
+
+        /*
+         * Create the MKMapView instance whose geometry will span the complete
+         * visible screen. Since a regular CGRect is passed to its constructor,
+         * the map could span any size and location on the screen depending on
+         * the application's needs.
+         */
+        rect.origin.x = rect.origin.y = 0;
+        MKMapView mapView = new MKMapView(rect);
+
+        /*
+         * Define a region to display. It consists of: 1. Center point:
+         * CLLocatoonCoordinate2D (latitude + longitude) 2. Span: the zoom, how
+         * far out you want to display from center point.
+         */
+        MKCoordinateRegion region;
+        MKCoordinateSpan span = new MKCoordinateSpan(0.2, 0.2);
+        CLLocationCoordinate2D location = new CLLocationCoordinate2D(37.779300, -122.419200);
+        region = new MKCoordinateRegion(location, span);
+        mapView.setRegion(region);
+
+        window.addSubview(mapView);
+
+        window.makeKeyAndVisible();
+    }
+
+    public static void main(String[] args) {
+        UIApplication.main(args, null, Map.class);
+    }
+
+}
---
+++ tutorial/ios/sensor/gps/src/java/org/xmlvm/tutorial/ios/sensor/gps/GPS.java
@@ -0,0 +1,125 @@
+/* Copyright (c) 2002-2011 by XMLVM.org
+ *
+ * Project Info:  http://www.xmlvm.org
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+package org.xmlvm.tutorial.ios.sensor.gps;
+
+import org.xmlvm.iphone.CGRect;
+import org.xmlvm.iphone.NSError;
+import org.xmlvm.iphone.UIApplication;
+import org.xmlvm.iphone.UIApplicationDelegate;
+import org.xmlvm.iphone.UIColor;
+import org.xmlvm.iphone.UILabel;
+import org.xmlvm.iphone.UIScreen;
+import org.xmlvm.iphone.UIWindow;
+import org.xmlvm.iphone.CLLocation;
+import org.xmlvm.iphone.CLLocationManager;
+import org.xmlvm.iphone.CLLocationManagerDelegate;
+import org.xmlvm.iphone.NSLog;
+
+/**
+ * The GPS application allows the user to see their current location in
+ * longitude and latitude. Both longitude and latitude will get updated when
+ * there is a change of location.
+ */
+public class GPS extends UIApplicationDelegate {
+
+    private UILabel labelLongitude;
+    private UILabel labelLatitude;
+
+
+    @Override
+    public void applicationDidFinishLaunching(UIApplication app) {
+        CGRect rect = UIScreen.mainScreen().getApplicationFrame();
+        UIWindow window = new UIWindow(rect);
+        window.setBackgroundColor(UIColor.whiteColor);
+
+        UILabel label = new UILabel(new CGRect(20, 20, 80, 30));
+        label.setText("Longitude:");
+        window.addSubview(label);
+
+        /*
+         * labelLongitude will display the current longitude.
+         */
+        labelLongitude = new UILabel(new CGRect(110, 20, 250, 30));
+        labelLongitude.setText("-");
+        window.addSubview(labelLongitude);
+
+        label = new UILabel(new CGRect(20, 50, 80, 30));
+        label.setText("Latitude:");
+        window.addSubview(label);
+
+        /*
+         * labelLatitude will display the current latitude.
+         */
+        labelLatitude = new UILabel(new CGRect(110, 50, 250, 30));
+        labelLatitude.setText("-");
+        window.addSubview(labelLatitude);
+
+        /*
+         * Create a Location Manager from the Core Location Framework.
+         */
+        CLLocationManager manager = new CLLocationManager();
+
+        /*
+         * Set a delegate that will be invoked whenever there is an update to
+         * the current location.
+         */
+        manager.setDelegate(new CLLocationManagerDelegate() {
+
+            @Override
+            public void didUpdateToLocation(CLLocationManager manager, CLLocation newLocation,
+                    CLLocation oldLocation) {
+                /*
+                 * Whenever the location updates, this callback will be called
+                 * by iOS. Retrieve the current longitude and latitude and
+                 * display them in the appropriate labels.
+                 */
+                labelLongitude.setText("" + newLocation.getCoordinate().longitude);
+                labelLatitude.setText("" + newLocation.getCoordinate().latitude);
+            }
+
+            @Override
+            public void didFailWithError(CLLocationManager manager, NSError error) {
+                super.didFailWithError(manager, error);
+                NSLog.log(error);
+            }
+
+        });
+
+        /*
+         * When iOS asks the user whether this application is allowed to use
+         * location services, it will also display the so-called "purpose" to
+         * the user as an explanation why the application requests this service.
+         */
+        manager.setPurpose("XMLVM GPS Demo");
+
+        /*
+         * Start delivering location updates to the delegate.
+         */
+        manager.startUpdatingLocation();
+
+        window.makeKeyAndVisible();
+    }
+
+    public static void main(String[] args) {
+        UIApplication.main(args, null, GPS.class);
+    }
+
+}
