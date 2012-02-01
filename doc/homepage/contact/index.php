fbef99871ef25a2609355358dbe031943ce5f5fc
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2012-02-01 20:30:57 +0000

    - Add mailing list to contacts page so that it's clear that this is the way to contact us for general questions about how to use XMLVM. - Also change the style of the red box, which also affects the main page, so that the link is always centered. Fails on IE <= 7, but that's ok :)
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@2140 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- doc/homepage/contact/index.php
+++ doc/homepage/contact/index.php
@@ -7,6 +7,15 @@
 ?>
 <h1>Contact</h1>
 
+<h2>Mailing list:</h2>
+<table><tr><td style="width:210px">
+  <div class="rounded_rectangle"><a target="_blank" href="https://lists.sourceforge.net/lists/listinfo/xmlvm-users">xmlvm-users</a>
+  </div>
+</td><td style="vertical-align:middle">
+For all questions about how to use XMLVM, please subscribe to and post on our mailing list. We and our community are happy to help you and try to respond as quickly as possible. Simply click the link on the left and follow the 
+instructions.
+</td></tr></table>
+
 <h2>XMLVM Core Team:</h2>
 <ul>
     <li><div id="arno">Arno Puder</div></li>
