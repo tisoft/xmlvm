eccb474d58c39470e1f5cb7f4b4f2d73e0715a95
shaeberling <shaeberling@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2012-03-20 18:03:46 +0000

    Removed e-mail addresses and role from core team.
    git-svn-id: http://xmlvm.svn.sourceforge.net/svnroot/xmlvm/trunk/xmlvm@2179 0874c2aa-48f6-4a0f-8a6b-e32f4465a257

--- doc/homepage/contact/index.php
+++ doc/homepage/contact/index.php
@@ -18,9 +18,9 @@ instructions.
 
 <h2>XMLVM Core Team:</h2>
 <ul>
-    <li><div id="arno">Arno Puder</div></li>
-    <li><div id="sascha">Sascha H&auml;berling</div></li>
-	  <li><div id="frusty">Wolfgang Korn</div></li>
+  <li><div id="arno">Arno Puder</div></li>
+  <li><div id="sascha">Sascha H&auml;berling</div></li>
+  <li><div id="frusty">Wolfgang Korn</div></li>
 </ul>
 
 <h2>Contributors:</h2>
@@ -33,28 +33,6 @@ instructions.
   <li>Paul Poley</li>
   <li>Alexander Seewald</li>
 </ul>
-
-
-<script type="text/javascript">
-<!--
-  var arno = 'Arno Puder &lt;arno<b>';
-  arno += '&#64;</b>xmlvm&#46';
-  arno += ';&#111;rg&gt; (Developer)';
-
-  var sascha = 'Sascha Haeberling &lt;sascha<b>';
-  sascha += '&#64;</b>xmlvm&#46';
-  sascha += ';&#111;rg&gt; (Developer)';
-  
-  var frusty = 'Wolfgang Korn &lt;wolfgang<b>';
-  frusty += '&#64;</b>xmlvm&#46';
-  frusty += ';&#111;rg&gt; (Developer)';
-  
-  document.getElementById('arno').innerHTML = arno;
-  document.getElementById('sascha').innerHTML = sascha;
-  document.getElementById('frusty').innerHTML = frusty;
-
-//-->
-</script>
 
 <?php
   include("../footer.html");
